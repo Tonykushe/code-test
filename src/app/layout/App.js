25e5fbe2ccb5ad156a73ec9aa6a57c735bb354a3
Tony Mugendi <mugendi.tony26@gmail.com>
2019-02-07 20:23:08 +0300

    complete

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -9,8 +9,6 @@ class App extends Component {
             <div className="App">
                 <Navbar />
                 <Users />
-                
-                
             </div>
         );
     }
